a7ea6d2d55f8908bda18f34e4075d0676df3e03c
jansona <alelipona121@gmail.com>
2019-06-27 15:34:05 +0800

    调整登录接口

--- BabyBack/src/main/java/com/example/demo/controller/LoginViewController.java
+++ BabyBack/src/main/java/com/example/demo/controller/LoginViewController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.ResponseBase;
 import com.example.demo.entity.User;
 import com.example.demo.reposity.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,21 +27,26 @@ public class LoginViewController {
      * @return
      */
     @RequestMapping("/login")
-    public String login(HttpServletRequest request) {
+    public ResponseBase login(HttpServletRequest request) {
+
+        ResponseBase responseBase;
+
         String account = request.getParameter("account");
         String password = request.getParameter("password");
         if (null == account || null == password) {
-            return "redirect:/";    // TODO 重定向地址填写
+            return new ResponseBase(20001, "登陆异常", null);    // TODO 重定向地址填写
         }
 
         // 前端传回的密码实际为用户输入的：用户名（小写）+ 密码（小写）组合的字符串生成的md5值
         // 此处先通过后台保存的正确的用户名和密码计算出正确的md5值，然后和前端传回来的作比较
 
+        User user;
         if(account.contains("@")){
-            rightPassword = userRepository.findByEmail(account).getPassWord();
+            user = userRepository.findByEmail(account);
         }else{
-            rightPassword = userRepository.findByTel(account).getPassWord();
+            user = userRepository.findByTel(account);
         }
+        rightPassword = user.getPassWord();
 
 //        User user = userRepository.findByAccount(account);
 //        rightUserName = user.getAccount();
@@ -50,13 +56,15 @@ public class LoginViewController {
         String realPassword = rightPassword;
 
         if (!password.equals(realPassword)) {
-            return "redirect:/";    // TODO 重定向地址填写
+            responseBase = new ResponseBase(20001, "账号或密码错误", null);    // TODO 重定向地址填写
+        }else{
+            responseBase = new ResponseBase(200, "登陆成功", user);
         }
 
         // 校验通过时，在session里放入一个标识
         // 后续通过session里是否存在该标识来判断用户是否登录
         request.getSession().setAttribute("account", account);
-        return "redirect:/welcome"; // TODO 重定向地址填写
+        return responseBase; // TODO 重定向地址填写
     }
 
     /**
@@ -66,7 +74,7 @@ public class LoginViewController {
      * @return
      */
     @RequestMapping("/logout")
-    public String loginOut(HttpServletRequest request) {
+    public String logout(HttpServletRequest request) {
         request.getSession().invalidate();
         return "redirect:/";    // TODO 重定向地址填写
     }
