09d2d8c458963bb548ab885bda4845636b5f4d81
jansona <alelipona121@gmail.com>
2019-06-29 09:59:30 +0800

    :sparkles: 后端加入短信验证模块

--- BabyBack/src/main/java/com/example/demo/controller/ArticleController.java
+++ BabyBack/src/main/java/com/example/demo/controller/ArticleController.java
@@ -38,8 +38,8 @@ public class ArticleController {
             article_insert = articleRepository.save(article);
             responseBase = new ResponseBase(200, "新增文章成功", article_insert);
         } catch (Exception e) {
-            responseBase = new ResponseBase(30001, "新增文章失败", article);
             e.printStackTrace();
+            responseBase = new ResponseBase(30001, "新增文章失败", article);
         }
 
         return responseBase;
---
+++ BabyBack/src/main/java/com/example/demo/controller/RegisterController.java
@@ -0,0 +1,63 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.ResponseBase;
+import com.example.demo.entity.User;
+import com.example.demo.reposity.UserRepository;
+import com.example.demo.utils.SMSSender;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@CrossOrigin
+@RestController
+public class RegisterController {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @ApiOperation(value = "发送验证码")
+    @PostMapping("/sms-verify")
+    public ResponseBase logout(@RequestBody String tel) {
+        ResponseBase responseBase;
+        try {
+            String code = SMSSender.sendSMS(tel);
+            stringRedisTemplate.opsForValue().set(tel, code);
+            responseBase = new ResponseBase(200, "验证码短信发送成功", null);
+        } catch (Exception e) {
+            e.printStackTrace();
+            responseBase = new ResponseBase(50001, "验证码短信发送异常", null);
+        }
+        return responseBase;
+    }
+
+    @ApiOperation(value = "验证并注册一个用户")
+    @PostMapping("/register")
+    public ResponseBase insertUser(@RequestBody User user, @RequestBody String code) {
+        ResponseBase responseBase;
+        try {
+            String realCode = stringRedisTemplate.opsForValue().get(user.getTel());
+
+            if (realCode.equals(code)) {
+                User user_saved = userRepository.save(user);
+                stringRedisTemplate.delete(user.getTel());
+                responseBase = new ResponseBase(200, "短信验证成功", user_saved);
+            } else {
+                responseBase = new ResponseBase(50003, "短信验证失败", null);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseBase(50002, "短信验证码验证异常", null);
+        }
+        return responseBase;
+    }
+
+}
