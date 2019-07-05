edf61932c17651fbecf2b7fc246677083336bf37
fansking <935802216@qq.com>
2019-07-05 20:10:51 +0800

    merge

--- baby_back_frontpage/src/pages/fastMatch/FastMatch.vue
+++ baby_back_frontpage/src/pages/fastMatch/FastMatch.vue
@@ -3,11 +3,13 @@
     <el-card>
       <el-tabs @tab-click="handleClick" v-model="tab">
         <el-tab-pane label="照片匹配" name="first">
-          <div class="clearfix">
+          <div slot="header" class="clearfix">
             <PicUpload :type="2" @return-data="getData"></PicUpload>
           </div>
           <div class="text item">
-            <div class="block"></div>
+            <div class="block">
+              <Pictures :datas="datas" :types="dataTypes" ref="pictures0" v-if="choosed===0"></Pictures>
+            </div>
           </div>
         </el-tab-pane>
         <el-tab-pane label="描述匹配" name="second">
@@ -15,27 +17,24 @@
           <br />
           <br />
           <el-col offset="20" span="4">
-          <el-button type="primary" size="small" style="float:right">开始匹配</el-button></el-col>
+            <el-button type="primary" size="small" style="float:right">开始匹配</el-button>
+          </el-col>
         </el-tab-pane>
       </el-tabs>
-      <!-- <div slot="header" class="clearfix">
-      <PicUpload :type="2" @return-data="getData">
-      </PicUpload>
-    </div>
-    <div class="text item">
-      <div class="block">
-      </div>
-      </div>-->
     </el-card>
   </div>
 </template>
 
 <script>
 import PicUpload from "@/pages/findChildren/PicUpload";
+import Pictures from "../faceWall/components/Pictures";
 export default {
   name: "FsatMatch",
   data() {
     return {
+      choosed: 0,
+      datas: [],
+      dataTypes: [],
       tab: "first",
       input: ""
     };
@@ -46,24 +45,36 @@ export default {
   methods: {
     getData(data) {
       console.log("waimian:", data);
+      if (data.rtnCode == 200) {
+        if (this.choosed == -1) this.choosed = 0;
+        this.datas = JSON.parse(JSON.stringify(data.data));
+        this.dataTypes = [];
+        for (let i = 0, len = this.datas.length; i < len; i++) {
+          this.dataTypes.push(this.choosed);
+        }
+        this.$refs.pictures0.reloadData();
+        let saved = this.choosed;
+        this.choosed = -1;
+        this.$nextTick(function() {
+          this.choosed = saved;
+        });
+      }
     },
     handleClick(tab, event) {
       console.log(tab, event);
     },
-    nlpMatch(){
-      if(this.input != ''){
-        console.log('do nlp task')
-      }else{
-         this.$notify({
-            message: "描述信息不能为空",
-            type: "warning",
-            duration: 1500,
-            offset: 50
-          });
+    nlpMatch() {
+      if (this.input != "") {
+        console.log("do nlp task");
+      } else {
+        this.$notify({
+          message: "描述信息不能为空",
+          type: "warning",
+          duration: 1500,
+          offset: 50
+        });
       }
-      
     }
-    
   }
 };
 </script>
